16ed42786c42ac785ca4849647c9af20367266a4
Namcha <Lanend18@gmail.com>
2023-04-21 18:50:51 +0700

    Javascript-HTML

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function getPyrmidArea(length,width,height) {
-    let base = length * width
-    let pyraminVolume = 1/3 * base * height
-    console.log(pyraminVolume)
-    return pyraminVolume
-}
+let content2 = document.getElementById('content-2')
+let textHtml = '<b>เกิดมาไม่เคยเจอใครเหมือนเธอ</b>'
+textHtml += ' <i>หลับฝันละเมอภาพเธอคอยหลอนทุกคืน</i> '
+content2.innerHTML = textHtml;
 
-let area1 = getPyrmidArea(2,2,3)
-let area2 = getPyrmidArea(4,5,6)
-
-console.log('Area 1 = ' + area1 + ' Area 2 = ' + area2)
 
