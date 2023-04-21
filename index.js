f75058a2f7ff75d486ba7127d1e79a430c961adb
Namcha <Lanend18@gmail.com>
2023-04-21 19:02:25 +0700

    Javascript-Event

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ textHtml += ' <i>หลับฝันละเมอภาพเธอคอย
 content2.innerHTML = textHtml;
 
 
+let discountButton = document.getElementById('discount-button')
+let message = document.getElementById('message')
+
+function showMessage() {
+    message.innerHTML = 'หมดเวลาสนุกแล้วสิ'
+}
+
+discountButton.addEventListener('click', showMessage)
